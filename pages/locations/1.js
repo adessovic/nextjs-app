0b36f0efb60ad9577e08f376557de03c59347a26
Aleks C <aleksandar.cincarevic@adesso-sweden.se>
2023-01-18 17:03:34 +0100

    post updates, date formatting

--- pages/locations/1.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-export default function(){
-    return(
-        <div>
-            <Link href="/locations">← Back</Link>
-            <h1>Location 1</h1>
-        </div>
-    )
-}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,12 @@
+import utilStyles from "../../styles/utils.module.css";
+
+import Head from "next/head";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -21,11 +25,19 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
